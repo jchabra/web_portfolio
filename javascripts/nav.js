fb274535e2404de0705e21cc9789c08a86d83f98
Jasmine <jchabra@gmail.com>
2014-11-14 16:55:31 -0500

    initial commit

---
+++ javascripts/nav.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  $('#navbuttons a').click(scroll);
+
+  $(window).scroll(showReturn);
+  $(window).resize(showReturn);
+  $('#returntotop').click(returnTop);
+
+});
+
+function scroll()
+{
+  $.scrollTo( this.hash, 900, { easing: 'easeOutQuad', axis: 'y' });
+  return false;
+  // need return false so that the href normally there doesn't fire off
+}
+
+function returnTop()
+{
+  $.scrollTo( "#container", 900, { easing: 'easeOutQuad', axis: 'y' });
+}
+
+function showReturn()
+{
+  if (($(this).scrollTop() > $('#portfolio').position().top) && ($('#about').children().children('.four').width() < 370))
+    {
+        $('#returntotop').fadeIn();
+    }
+  else
+    {
+        $('#returntotop').fadeOut();
+    }
+}
+
+
+
