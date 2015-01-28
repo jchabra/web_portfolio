eef2667326a5ec753f463b2fceb7199ba43b11ff
Jasmine <jchabra@gmail.com>
2015-01-27 19:58:27 -0500

    updated the functionality of the contact form

--- index.php
+++ index.php
@@ -5,14 +5,7 @@
   $human = $_POST['human'];
   $formcontent=" From: $name \n Email: $email  \n Message: $message";
   $recipient = "jchabra@gmail.com";
-  $subject = "Contact Form";
+  $subject = "Somebody contacted you from your website!";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-  if ($_POST['submit']) {
-    if (successful) {
-      print "<p>Your email has been sent; Thank you.</p>";
-    } else {
-      print "<p>Sorry, there was a problem sending the email.</p>";
-    }
-  }
 ?>
--- javascripts/contact.js
+++ javascripts/contact.js
@@ -1,5 +1,4 @@
 //Form Validation
-
 $('#contact_form').validate({
   rules: {
     name: 'required',
@@ -21,5 +20,25 @@ $('#contact_form').validate({
   },
   errorPlacement: function(error, element) {
     error.insertBefore( element );
+  },
+  submitHandler: function(form) {
+    $(form).ajaxSubmit({
+      type:"POST",
+      data: $(form).serialize(),
+      url:"process.php",
+      success: function() {
+        $('#contact_form :input').attr('disabled', 'disabled');
+        $('#contact_form').fadeTo( "slow", 0.15, function() {
+          $(this).find(':input').attr('disabled', 'disabled');
+          $(this).find('label').css('cursor','default');
+          $('#success').fadeIn();
+        });
+      },
+      error: function() {
+        $('#contact_form').fadeTo( "slow", 0.15, function() {
+          $('#error').fadeIn();
+        });
+      }
+    });
   }
 });
